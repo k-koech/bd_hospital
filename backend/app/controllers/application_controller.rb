2cbff5160c032b4a96c6a8b6e0d194d9dc58670f
k-koech <koechkelvin97@gmail.com>
2022-10-16 10:23:29 +0300

    deploy

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
     options "*" do
         # response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS, HEAD"
         response.headers["Access-Control-Allow-Methods"] = "GET, PUT, POST, DELETE, OPTIONS, HEAD"
-        response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
         response.headers["Access-Control-Allow-Origin"] = "*"
         200
     end 
--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -6,5 +6,18 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
+########################
+configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+   
+    ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
+   end
 # Require in all files in 'app' directory
 require_all 'app'
